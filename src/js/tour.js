67aafcf3eea5f4e58c7604a0de50f7998ebf29fc
James Lave <jlave.dev@gmail.com>
2020-02-07 17:09:55 -0600

    Add tour step when SpeechRecognition API not available

--- src/js/tour.js
+++ src/js/tour.js
@@ -6,54 +6,63 @@ const tour = new Shepherd.Tour({
   },
 });
 
-tour.addStep('welcome', {
-  title: '👋 Welcome!',
-  text: 'If speaking in public or on the phone causes you to panic and talk too quickly, this app can help remind you to remain calm! Continue on for a quick tour.',
-  advanceOn: '.docs-link click',
-  showCancelLink: true,
-});
+if (!window.SpeechRecognition && !window.webkitSpeechRecognition) {
+  tour.addStep('speech-check', {
+    title: '🚨 Uh oh',
+    text: 'Speech recognition is not available in your browser! Please use this site in Google Chrome for it to work properly.',
+  });
+} else {
+  tour.addStep('welcome', {
+    title: '👋 Welcome!',
+    text: 'If speaking in public or on the phone causes you to panic and talk too quickly, this app can help remind you to remain calm! Continue on for a quick tour.',
+    advanceOn: '.docs-link click',
+    showCancelLink: true,
+  });
 
-tour.addStep('lang', {
-  title: '🌐 Multi-Language Support',
-  text: 'This app uses your browser\'s default language. If you\'d like to speak in a different language, select it from the dropdown menu above.',
-  attachTo: '.navbar-item bottom',
-  advanceOn: '.docs-link click',
-  showCancelLink: true,
-});
+  tour.addStep('lang', {
+    title: '🌐 Multi-Language Support',
+    text: 'This app uses your browser\'s default language. If you\'d like to speak in a different language, select it from the dropdown menu above.',
+    attachTo: '.navbar-item bottom',
+    advanceOn: '.docs-link click',
+    showCancelLink: true,
+  });
 
-tour.addStep('wpm', {
-  title: '⏲️ Words Per Minute Display',
-  text: 'Here you can see the rate (in number of words per minute) of your last recognized speech snippet.',
-  attachTo: '#wpm-display bottom',
-  advanceOn: '.docs-link click',
-});
+  tour.addStep('wpm', {
+    title: '⏲️ Words Per Minute Display',
+    text: 'Here you can see the rate (in number of words per minute) of your last recognized speech snippet.',
+    attachTo: '#wpm-display bottom',
+    advanceOn: '.docs-link click',
+  });
 
-tour.addStep('gauge', {
-  title: '📊 WPM Gauge',
-  text: 'Your speaking rate is also visually represented by this gauge. Try to keep it in the green!',
-  attachTo: '#gauge top',
-  advanceOn: '.docs-link click',
-});
+  tour.addStep('gauge', {
+    title: '📊 WPM Gauge',
+    text: 'Your speaking rate is also visually represented by this gauge. Try to keep it in the green!',
+    attachTo: '#gauge top',
+    advanceOn: '.docs-link click',
+  });
 
-tour.addStep('voice', {
-  title: '🎙️ Voice Detection Indicator',
-  text: 'This icon appears when the browser has detected speech and is interpreting it. Speak louder or move closer to your microphone if this icon is unstable.',
-  attachTo: '#voice-indicator top',
-  advanceOn: '.docs-link click',
-  when: {
-    show() {
-      document.querySelector('.loader-inner').classList.remove('hidden');
+  tour.addStep('voice', {
+    title: '🎙️ Voice Detection Indicator',
+    text: 'This icon appears when the browser has detected speech and is interpreting it. Speak louder or move closer to your microphone if this icon is unstable.',
+    attachTo: '#voice-indicator top',
+    advanceOn: '.docs-link click',
+    when: {
+      show() {
+        document.querySelector('.loader-inner').classList.remove('hidden');
+      },
+      hide() {
+        document.querySelector('.loader-inner').classList.add('hidden');
+      },
     },
-    hide() {
-      document.querySelector('.loader-inner').classList.add('hidden');
-    },
-  },
-});
+  });
 
-// check local storage for visited item
-// only run tour if not visited previously
-if (!localStorage.getItem('visited')) {
+  tour.on('complete', () => {
+    localStorage.setItem('completed', 'true');
+  });
+}
+
+// check local storage for completed item
+// only run tour if not completed previously
+if (!localStorage.getItem('completed')) {
   tour.start();
 }
-localStorage.setItem('visited', 'true');
-
